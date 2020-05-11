aa9e561e81bb420ddf860380d9cbca2763f0561b
Saya <chu121su12@gmail.com>
2020-05-11 23:54:50 +0800

    missed a function

--- src/Illuminate/Support/Str.php
+++ src/Illuminate/Support/Str.php
@@ -279,7 +279,22 @@ class Str
      */
     public static function isAscii($value)
     {
+        return UTF8::is_ascii((string) $value);
+    }
 
+    /**
+     * Determine if a given string is a valid UUID.
+     *
+     * @param  string  $value
+     * @return bool
+     */
+    public static function isUuid($value)
+    {
+        if (! is_string($value)) {
+            return false;
+        }
+
+        return preg_match('/^[\da-f]{8}-[\da-f]{4}-[\da-f]{4}-[\da-f]{4}-[\da-f]{12}$/iD', $value) > 0;
     }
 
     /**
