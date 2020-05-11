0001216acb3f0254c2cc4390780a2574a5a89e52
Saya <chu121su12@gmail.com>
2020-05-11 21:47:01 +0800

    do not extend throwable

--- cr/patch/symfony-mime/mime-5.0.8/Exception/ExceptionInterface.php
+++ cr/patch/symfony-mime/mime-5.0.8/Exception/ExceptionInterface.php
@@ -14,6 +14,6 @@ namespace Symfony\Component\Mime\Exception;
 /**
  * @author Fabien Potencier <fabien@symfony.com>
  */
-interface ExceptionInterface extends \Throwable
+interface ExceptionInterface
 {
 }
