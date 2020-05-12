973e3ca352e48aab303273a9398f809efa6c48a7
Saya <chu121su12@gmail.com>
2020-05-12 15:18:11 +0800

    override with symfony 5.x uploaded file signatures

--- src/Illuminate/Http/UploadedFile.php
+++ src/Illuminate/Http/UploadedFile.php
@@ -15,6 +15,35 @@ class UploadedFile extends SymfonyUploadedFile
     use FileHelpers, Macroable;
 
     /**
+     * Accepts the information of the uploaded file as provided by the PHP global $_FILES.
+     *
+     * The file object is only created when the uploaded file is valid (i.e. when the
+     * isValid() method returns true). Otherwise the only methods that could be called
+     * on an UploadedFile instance are:
+     *
+     *   * getClientOriginalName,
+     *   * getClientMimeType,
+     *   * isValid,
+     *   * getError.
+     *
+     * Calling any other method on an non-valid instance will cause an unpredictable result.
+     *
+     * @param string      $path         The full temporary path to the file
+     * @param string      $originalName The original file name of the uploaded file
+     * @param string|null $mimeType     The type of the file as provided by PHP; null defaults to application/octet-stream
+     * @param int|null    $error        The error constant of the upload (one of PHP's UPLOAD_ERR_XXX constants); null defaults to UPLOAD_ERR_OK
+     * @param bool        $test         Whether the test mode is active
+     *                                  Local files are used in test mode hence the code should not enforce HTTP uploads
+     *
+     * @throws FileException         If file_uploads is disabled
+     * @throws FileNotFoundException If the file does not exist
+     */
+    public function __construct($path, $originalName, $mimeType = null, $error = null, $test = false, $unused = null)
+    {
+        parent::__construct($path, $originalName, $mimeType, null, $error, $test);
+    }
+
+    /**
      * Begin creating a new file fake.
      *
      * @return \Illuminate\Http\Testing\FileFactory
@@ -102,6 +131,19 @@ class UploadedFile extends SymfonyUploadedFile
         }
 
         return file_get_contents($this->getPathname());
+    }
+
+    /**
+     * Returns the file size.
+     *
+     * It is extracted from the request from which the file has been uploaded.
+     * Then it should not be considered as a safe value.
+     *
+     * @return int|null The file size
+     */
+    public function getClientSize()
+    {
+        return $this->getSize();
     }
 
     /**
