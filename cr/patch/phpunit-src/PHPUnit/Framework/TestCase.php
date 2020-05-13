ccbcede66e277777d6b7fcb6a7e5e7f952697dfb
Saya <chu121su12@gmail.com>
2020-05-13 10:06:31 +0800

    auth exception code is 0

--- cr/patch/phpunit-src/PHPUnit/Framework/TestCase.php
+++ cr/patch/phpunit-src/PHPUnit/Framework/TestCase.php
@@ -5,20 +5,4 @@ namespace PHPUnit\Framework;
 abstract class TestCase extends \PHPUnit_Framework_TestCase
 {
     use PhpUnit8Expect;
-
-    /**
-     * @param null|int|string $code
-     *
-     * @throws PHPUnit_Framework_Exception
-     */
-    public function expectExceptionCode($code)
-    {
-        try {
-            parent::expectExceptionCode($code);
-        } catch (\PHPUnit_Framework_Exception $e) {
-            if (!(is_null($code) && $e->getMessage() === 'Argument #1 (No Value) of PHPUnit_Framework_TestCase::expectExceptionCode() must be a integer or string"')) {
-                throw $e;
-            }
-        }
-    }
 }
