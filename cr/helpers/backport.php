4b92d91d37cb9ba795cefe230bd0ae81fe9f843c
Saya <chu121su12@gmail.com>
2020-06-22 20:50:05 +0800

    reorder function to save memory on php5

--- cr/helpers/backport.php
+++ cr/helpers/backport.php
@@ -4,16 +4,11 @@ if (! function_exists('backport_json_decode'))
 {
     function backport_json_decode($json, $assoc = false, $depth = 512, $options = 0)
     {
-        $decoded = json_decode($json, $assoc, $depth, $options);
-
-        if (json_last_error() === JSON_ERROR_NONE) {
-            // https://www.php.net/manual/en/function.json-decode 7.0.0 changes
-            if ((string) $json === '') {
-                json_decode('-');
-                return null;
-            }
+        // https://www.php.net/manual/en/function.json-decode 7.0.0 changes
+        if ((string) $json === '') {
+            return json_decode('-');
         }
 
-        return $decoded;
+        return json_decode($json, $assoc, $depth, $options);
     }
 }
