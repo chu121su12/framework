911b78b02e5ef528087b7b124cbfb16e0fa65fa4
Saya <chu121su12@gmail.com>
2020-08-20 22:53:08 +0800

    revert phpunit exception

--- tests/Foundation/FoundationInteractsWithDatabaseTest.php
+++ tests/Foundation/FoundationInteractsWithDatabaseTest.php
@@ -8,8 +8,10 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Query\Builder;
 use Illuminate\Foundation\Testing\Concerns\InteractsWithDatabase;
 use Mockery as m;
-use PHPUnit\Framework\ExpectationFailedException;
 use PHPUnit\Framework\TestCase;
+
+use PHPUnit_Framework_ExpectationFailedException as ExpectationFailedException;
+// use PHPUnit\Framework\ExpectationFailedException;
 
 class FoundationInteractsWithDatabaseTest extends TestCase
 {
