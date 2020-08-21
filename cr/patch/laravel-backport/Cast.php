8c1fda1d8b1c932707cbcff4d839dd1517ba762d
Saya <chu121su12@gmail.com>
2020-08-21 10:45:10 +0800

    update laravel ui

--- cr/patch/laravel-backport/Cast.php
+++ cr/patch/laravel-backport/Cast.php
@@ -4,48 +4,4 @@ namespace CR\LaravelBackport;
 
 class Cast
 {
-    public static function _array($value, $default = null)
-    {
-        if (func_num_args() === 2 && is_null($value)) {
-            return $default;
-        }
-
-        return (array) $value;
-    }
-
-    public static function _bool($value, $default = null)
-    {
-        if (func_num_args() === 2 && is_null($value)) {
-            return $default;
-        }
-
-        return (bool) $value;
-    }
-
-    public static function _float($value, $default = null)
-    {
-        if (func_num_args() === 2 && is_null($value)) {
-            return $default;
-        }
-
-        return (float) $value;
-    }
-
-    public static function _int($value, $default = null)
-    {
-        if (func_num_args() === 2 && is_null($value)) {
-            return $default;
-        }
-
-        return (int) $value;
-    }
-
-    public static function _string($value, $default = null)
-    {
-        if (func_num_args() === 2 && is_null($value)) {
-            return $default;
-        }
-
-        return (string) $value;
-    }
 }
