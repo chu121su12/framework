77148397c864ea831a5787abe7b9744c9162f07a
Saya <chu121su12@gmail.com>
2020-08-21 10:38:59 +0800

    punctuation

--- cr/helpers/tests.php
+++ cr/helpers/tests.php
@@ -23,7 +23,7 @@ if (! function_exists('phpunit_assert_v5_skip_test'))
     function phpunit_assert_v5_skip_test($instance)
     {
         if (phpunit_major_version() <= 5) {
-            $instance->markTestSkipped('Cannot run test with PHPUnit 5');
+            $instance->markTestSkipped('Cannot run test with PHPUnit 5.');
         }
     }
 }
