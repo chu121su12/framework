d5788ef35d93d0716d4984c9f9cd241aa1e91e2c
Saya <chu121su12@gmail.com>
2020-08-26 18:33:06 +0800

    use shell exec

--- src/Illuminate/Filesystem/Filesystem.php
+++ src/Illuminate/Filesystem/Filesystem.php
@@ -287,7 +287,9 @@ class Filesystem
     public function link($target, $link)
     {
         if (! windows_os()) {
-            return symlink($target, $link);
+            exec("ln -s ".escapeshellarg($target).' '.escapeshellarg($link));
+            return;
+            // return symlink($target, $link);
         }
 
         $mode = $this->isDirectory($target) ? 'J' : 'H';
