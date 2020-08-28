569a98c78bfc26d13624e2518b2f7da09776fd36
Saya <chu121su12@gmail.com>
2020-08-28 10:25:30 +0800

    patch phpunit traits

--- cr/patch/phpunit-src/PHPUnit/Framework/PhpUnit8Expect.php
+++ cr/patch/phpunit-src/PHPUnit/Framework/PhpUnit8Expect.php
@@ -2,18 +2,25 @@
 
 namespace PHPUnit\Framework;
 
-trait PhpUnit8Expect
-{
-    public function expectExceptionMessageMatches($regularExpression)
+if (phpunit_major_version() > 5) {
+    trait PhpUnit8Expect
     {
-        $this->expectedExceptionMessageRegExp = $regularExpression;
     }
 
-    public function expectExceptionObject($exception)
+} else {
+    trait PhpUnit8Expect
     {
-        $this->expectException(\get_class($exception));
-        $this->expectExceptionMessage($exception->getMessage());
-        $this->expectExceptionCode($exception->getCode());
-    }
+        public function expectExceptionMessageMatches($regularExpression)
+        {
+            $this->expectedExceptionMessageRegExp = $regularExpression;
+        }
 
+        public function expectExceptionObject($exception)
+        {
+            $this->expectException(\get_class($exception));
+            $this->expectExceptionMessage($exception->getMessage());
+            $this->expectExceptionCode($exception->getCode());
+        }
+
+    }
 }
