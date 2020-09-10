a9c219d2b183cb7983ee28633b893ae8002509fb
Saya <chu121su12@gmail.com>
2020-09-10 09:46:51 +0800

    call internal __set_state_

--- cr/patch/carbon/carbon/Patch/SetState.php
+++ cr/patch/carbon/carbon/Patch/SetState.php
@@ -17,20 +17,6 @@ if (version_compare(PHP_VERSION, '7.0.0', '<'))
         {
             return static::__set_state_(...func_get_args());
         }
-
-        public static function __set_state_($dump)
-        {
-            if (is_string($dump)) {
-                return static::parse($dump);
-            }
-
-            /** @var \DateTimeInterface $date */
-            $date = get_parent_class(static::class) && method_exists(parent::class, '__set_state')
-                ? parent::__set_state((array) $dump)
-                : (object) $dump;
-
-            return static::instance($date);
-        }
     }
 }
 else
@@ -46,16 +32,7 @@ else
          */
         public static function __set_state($dump)
         {
-            if (is_string($dump)) {
-                return static::parse($dump);
-            }
-
-            /** @var \DateTimeInterface $date */
-            $date = get_parent_class(static::class) && method_exists(parent::class, '__set_state')
-                ? parent::__set_state((array) $dump)
-                : (object) $dump;
-
-            return static::instance($date);
+            return static::__set_state_(...func_get_args());
         }
     }
 }
