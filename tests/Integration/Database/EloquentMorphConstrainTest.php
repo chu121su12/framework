4553e9ec130683f2487c72216968fb81ea66120a
Saya <chu121su12@gmail.com>
2020-11-17 00:45:44 +0800

    bc test

--- tests/Integration/Database/EloquentMorphConstrainTest.php
+++ tests/Integration/Database/EloquentMorphConstrainTest.php
@@ -13,7 +13,7 @@ use Illuminate\Tests\Integration\Database\DatabaseTestCase;
  */
 class EloquentMorphConstrainTest extends DatabaseTestCase
 {
-    protected function setUp(): void
+    protected function setUp()
     {
         parent::setUp();
 
