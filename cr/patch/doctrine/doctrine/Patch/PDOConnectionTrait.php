12947e9636ba32fd42bf3616b0fd40bdca5fed62
Saya <chu121su12@gmail.com>
2021-01-04 10:34:46 +0800

    fix doctrine patch

--- cr/patch/doctrine/doctrine/Patch/PDOConnectionTrait.php
+++ cr/patch/doctrine/doctrine/Patch/PDOConnectionTrait.php
@@ -2,7 +2,7 @@
 
 namespace Doctrine\Patch;
 
-if (\version_compare(\PHP_VERSION, '7.0.0', '<')) {
+if (\version_compare(\PHP_VERSION, '8.0.0', '<')) {
     require_once __DIR__ . '/../../bootstrapper/PDOConnectionTrait.php';
 } else {
     require_once __DIR__ . '/../../bootstrapper/php8/PDOConnectionTrait.php';
