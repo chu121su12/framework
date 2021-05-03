f8223ec1b112d85eba6ab939a13c1e7e0036bb8e
Saya <chu121su12@gmail.com>
2021-05-03 22:51:18 +0800

    wip

--- cr/patch/orchestra-testbench-core/testbench-core/src/Concerns/Database/WithSqlite.php
+++ cr/patch/orchestra-testbench-core/testbench-core/src/Concerns/Database/WithSqlite.php
@@ -9,26 +9,30 @@ use Illuminate\Database\Schema\SQLiteBuilder;
 use Illuminate\Database\SQLiteConnection;
 use Illuminate\Support\Fluent;
 
-class WithSqlite_hotfixForSqliteSchemaBuilder extends SQLiteConnection {
+class WithSqlite_hotfix_Blueprint extends Blueprint {
+                                public function dropForeign($index)
+                                {
+                                    return new Fluent();
+                                }
+                            }
+
+class WithSqlite_hotfix_SQLiteBuilder extends SQLiteBuilder {
+                        protected function createBlueprint($table, Closure $callback = null)
+                        {
+                            return new WithSqlite_hotfix_Blueprint($table, $callback);
+                        }
+                    }
+
+class WithSqlite_hotfix_SQLiteConnection extends SQLiteConnection {
                 public function getSchemaBuilder()
                 {
                     if ($this->schemaGrammar === null) {
                         $this->useDefaultSchemaGrammar();
                     }
 
-                    return new class($this) extends SQLiteBuilder {
-                        protected function createBlueprint($table, Closure $callback = null)
-                        {
-                            return new class($table, $callback) extends Blueprint {
-                                public function dropForeign($index)
-                                {
-                                    return new Fluent();
-                                }
-                            };
-                        }
-                    };
+                    return new WithSqlite_hotfix_SQLiteBuilder($this);
                 }
-            };
+            }
 
 trait WithSqlite
 {
@@ -40,7 +44,7 @@ trait WithSqlite
     protected function hotfixForSqliteSchemaBuilder()
     {
         Connection::resolverFor('sqlite', static function ($connection, $database, $prefix, $config) {
-            return new WithSqlite_hotfixForSqliteSchemaBuilder($connection, $database, $prefix, $config);
+            return new WithSqlite_hotfix_SQLiteConnection($connection, $database, $prefix, $config);
         });
     }
 }
