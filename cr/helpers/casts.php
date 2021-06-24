26668c17b495c089f86c7b3235fe2f50e7aaf59a
Saya <chu121su12@gmail.com>
2021-06-25 07:28:37 +0800

    casts

--- cr/helpers/casts.php
+++ cr/helpers/casts.php
@@ -158,3 +158,66 @@ if (! \function_exists('cast_to_string')) {
         throw new TypeError;
     }
 }
+
+if (! \function_exists('cast_to_bools')) {
+    function cast_to_bools($strict = false, array $values)
+    {
+        return \array_map(function ($value) use ($strict) {
+            return cast_to_bool($value, null, $strict);
+        }, $values);
+    }
+}
+
+if (! \function_exists('cast_to_callables')) {
+    function cast_to_callables(array $values)
+    {
+        return \array_map(function ($value) {
+            return cast_to_callable($value);
+        }, $values);
+    }
+}
+
+if (! \function_exists('cast_to_floats')) {
+    function cast_to_floats($strict = false, array $values)
+    {
+        return \array_map(function ($value) use ($strict) {
+            return cast_to_float($value, null, $strict);
+        }, $values);
+    }
+}
+
+if (! \function_exists('cast_to_ints')) {
+    function cast_to_ints($strict = false, array $values)
+    {
+        return \array_map(function ($value) use ($strict) {
+            return cast_to_int($value, null, $strict);
+        }, $values);
+    }
+}
+
+if (! \function_exists('cast_to_iterables')) {
+    function cast_to_iterables(array $values)
+    {
+        return \array_map(function ($value) use ($strict) {
+            return cast_to_iterable($value);
+        }, $values);
+    }
+}
+
+if (! \function_exists('cast_to_objects')) {
+    function cast_to_objects($strict = false, array $values)
+    {
+        return \array_map(function ($value) use ($strict) {
+            return cast_to_object($value, null, $strict);
+        }, $values);
+    }
+}
+
+if (! \function_exists('cast_to_strings')) {
+    function cast_to_strings($strict = false, array $values)
+    {
+        return \array_map(function ($value) use ($strict) {
+            return cast_to_string($value, null, $strict);
+        }, $values);
+    }
+}
