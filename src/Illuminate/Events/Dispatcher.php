c49fa02d8f5904afaae57bf3ec09b3ac332901c3
Saya <chu121su12@gmail.com>
2021-07-16 12:32:43 +0800

    fix

--- src/Illuminate/Events/Dispatcher.php
+++ src/Illuminate/Events/Dispatcher.php
@@ -572,7 +572,7 @@ class Dispatcher implements DispatcherContract
 
         $connection = $this->resolveQueue()->connection(method_exists($listener, 'viaConnection')
                     ? $listener->viaConnection()
-                    : isset($listener->connection) ? $listener->connection : null);
+                    : (isset($listener->connection) ? $listener->connection : null));
 
         $queue = method_exists($listener, 'viaQueue')
                     ? $listener->viaQueue()
