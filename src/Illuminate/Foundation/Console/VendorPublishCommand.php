98a589894b9b2c88248376d94311efbe96fdc37a
Saya <chu121su12@gmail.com>
2021-11-29 21:03:42 +0800

    Revert "[9.x] Fixing the Error : Class "League\Flysystem\Adapter\Local" not found (#36407)"
    This reverts commit 347dc5bd5a3ca3c441bfb5496f27eef2164fdcb6.

--- src/Illuminate/Foundation/Console/VendorPublishCommand.php
+++ src/Illuminate/Foundation/Console/VendorPublishCommand.php
@@ -7,8 +7,8 @@ use Illuminate\Filesystem\Filesystem;
 use Illuminate\Foundation\Events\VendorTagPublished;
 use Illuminate\Support\Arr;
 use Illuminate\Support\ServiceProvider;
+use League\Flysystem\Adapter\Local as LocalAdapter;
 use League\Flysystem\Filesystem as Flysystem;
-use League\Flysystem\Local\LocalFilesystemAdapter as LocalAdapter;
 use League\Flysystem\MountManager;
 
 class VendorPublishCommand extends Command
@@ -259,8 +259,8 @@ class VendorPublishCommand extends Command
     protected function moveManagedFiles($manager)
     {
         foreach ($manager->listContents('from://', true) as $file) {
-            if ($file['type'] === 'file' && (! $manager->fileExists('to://'.$file['path']) || $this->option('force'))) {
-                $manager->write('to://'.$file['path'], $manager->read('from://'.$file['path']));
+            if ($file['type'] === 'file' && (! $manager->has('to://'.$file['path']) || $this->option('force'))) {
+                $manager->put('to://'.$file['path'], $manager->read('from://'.$file['path']));
             }
         }
     }
