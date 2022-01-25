6c4af561791d984273055443d3ea87b95100f239
Saya <chu121su12@gmail.com>
2022-01-26 01:20:16 +0800

    disable share button by default

--- cr/ext/spatie/laravel-ignition/config/ignition.php
+++ cr/ext/spatie/laravel-ignition/config/ignition.php
@@ -60,7 +60,8 @@ return [
     |
     */
 
-    'enable_share_button' => env('IGNITION_SHARING_ENABLED', true),
+    // 'enable_share_button' => env('IGNITION_SHARING_ENABLED', true),
+    'enable_share_button' => env('IGNITION_SHARING_ENABLED', false),
 
     /*
     |--------------------------------------------------------------------------
