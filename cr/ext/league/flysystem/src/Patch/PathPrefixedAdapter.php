f86955a4c2fc717cb19242053fa9a7e0c73c1323
Saya <chu121su12@gmail.com>
2022-09-20 12:40:33 +0800

    bc

--- cr/ext/league/flysystem/src/Patch/PathPrefixedAdapter.php
+++ cr/ext/league/flysystem/src/Patch/PathPrefixedAdapter.php
@@ -1,32 +1,20 @@
 <?php
 
-namespace League\Flysystem\PathPrefixing;
+namespace League\Flysystem\Patch;
 
-use Generator;
+use League\Flysystem\AdapterInterface as FilesystemAdapter;
 use League\Flysystem\Config;
-use League\Flysystem\FileAttributes;
-use League\Flysystem\FilesystemAdapter;
-use League\Flysystem\PathPrefixer;
-use League\Flysystem\UnableToCheckDirectoryExistence;
-use League\Flysystem\UnableToCheckFileExistence;
-use League\Flysystem\UnableToCopyFile;
-use League\Flysystem\UnableToCreateDirectory;
-use League\Flysystem\UnableToDeleteDirectory;
-use League\Flysystem\UnableToDeleteFile;
-use League\Flysystem\UnableToMoveFile;
-use League\Flysystem\UnableToReadFile;
-use League\Flysystem\UnableToRetrieveMetadata;
-use League\Flysystem\UnableToSetVisibility;
-use League\Flysystem\UnableToWriteFile;
 use Throwable;
 
 class PathPrefixedAdapter implements FilesystemAdapter
 {
-    protected FilesystemAdapter $adapter;
-    private PathPrefixer $prefix;
+    protected /*FilesystemAdapter */$adapter;
+    private /*PathPrefixer */$prefix;
 
-    public function __construct(FilesystemAdapter $adapter, string $prefix)
+    public function __construct(FilesystemAdapter $adapter, /*string */$prefix)
     {
+        $prefix = cast_to_string($prefix);
+
         if ($prefix === '') {
             throw new \InvalidArgumentException('The prefix must not be empty.');
         }
@@ -35,154 +23,349 @@ class PathPrefixedAdapter implements FilesystemAdapter
         $this->prefix = new PathPrefixer($prefix);
     }
 
-    public function read(string $location): string
+    public function read(/*string */$location)/*: string*/
     {
+        $location = cast_to_string($location);
+
         try {
             return $this->adapter->read($this->prefix->prefixPath($location));
+        } catch (\Exception $previous) {
+        } catch (\ErrorException $previous) {
         } catch (Throwable $previous) {
-            throw UnableToReadFile::fromLocation($location, $previous->getMessage(), $previous);
+            // throw UnableToReadFile::fromLocation($location, $previous->getMessage(), $previous);
+        }
+
+        if (isset($previous)) {
+            throw new UnableToReadFile($previous->getMessage(), $previous->getCode(), $previous);
         }
     }
 
-    public function readStream(string $location)
+    public function readStream(/*string */$location)
     {
+        $location = cast_to_string($location);
+
         try {
             return $this->adapter->readStream($this->prefix->prefixPath($location));
+        } catch (\Exception $previous) {
+        } catch (\ErrorException $previous) {
         } catch (Throwable $previous) {
-            throw UnableToReadFile::fromLocation($location, $previous->getMessage(), $previous);
+            // throw UnableToReadFile::fromLocation($location, $previous->getMessage(), $previous);
+        }
+
+        if (isset($previous)) {
+            throw new UnableToReadFile($previous->getMessage(), $previous->getCode(), $previous);
         }
     }
 
-    public function listContents(string $location, bool $deep): Generator
+    public function listContents(/*string */$location = '', /*bool */$deep = false)/*: Generator*/
     {
+        $location = cast_to_string($location);
+
+        $deep = cast_to_bool($deep);
+
         foreach ($this->adapter->listContents($this->prefix->prefixPath($location), $deep) as $attributes) {
             yield $attributes->withPath($this->prefix->stripPrefix($attributes->path()));
         }
     }
 
-    public function fileExists(string $location): bool
+    public function fileExists(/*string */$location)/*: bool*/
     {
+        $location = cast_to_string($location);
+
         try {
             return $this->adapter->fileExists($this->prefix->prefixPath($location));
+        } catch (\Exception $previous) {
+        } catch (\ErrorException $previous) {
         } catch (Throwable $previous) {
-            throw UnableToCheckFileExistence::forLocation($location, $previous);
+            // throw UnableToCheckFileExistence::forLocation($location, $previous);
+        }
+
+        if (isset($previous)) {
+            throw new UnableToCheckFileExistence($previous->getMessage(), $previous->getCode(), $previous);
         }
     }
 
-    public function directoryExists(string $location): bool
+    public function directoryExists(/*string */$location)/*: bool*/
     {
+        $location = cast_to_string($location);
+
         try {
             return $this->adapter->directoryExists($this->prefix->prefixPath($location));
+        } catch (\Exception $previous) {
+        } catch (\ErrorException $previous) {
         } catch (Throwable $previous) {
-            throw UnableToCheckDirectoryExistence::forLocation($location, $previous);
+            // throw UnableToCheckDirectoryExistence::forLocation($location, $previous);
+        }
+
+        if (isset($previous)) {
+            throw new UnableToCheckDirectoryExistence($previous->getMessage(), $previous->getCode(), $previous);
         }
     }
 
-    public function lastModified(string $path): FileAttributes
+    public function lastModified(/*string */$path)/*: FileAttributes*/
     {
+        $path = cast_to_string($path);
+
         try {
             return $this->adapter->lastModified($this->prefix->prefixPath($path));
+        } catch (\Exception $previous) {
+        } catch (\ErrorException $previous) {
         } catch (Throwable $previous) {
-            throw UnableToRetrieveMetadata::lastModified($path, $previous->getMessage(), $previous);
+            // throw UnableToRetrieveMetadata::lastModified($path, $previous->getMessage(), $previous);
+        }
+
+        if (isset($previous)) {
+            throw new UnableToRetrieveMetadata($previous->getMessage(), $previous->getCode(), $previous);
         }
     }
 
-    public function fileSize(string $path): FileAttributes
+    public function fileSize(/*string */$path)/*: FileAttributes*/
     {
+        $path = cast_to_string($path);
+
         try {
             return $this->adapter->fileSize($this->prefix->prefixPath($path));
+        } catch (\Exception $previous) {
+        } catch (\ErrorException $previous) {
         } catch (Throwable $previous) {
-            throw UnableToRetrieveMetadata::fileSize($path, $previous->getMessage(), $previous);
+            // throw UnableToRetrieveMetadata::fileSize($path, $previous->getMessage(), $previous);
+        }
+
+        if (isset($previous)) {
+            throw new UnableToRetrieveMetadata($previous->getMessage(), $previous->getCode(), $previous);
         }
     }
 
-    public function mimeType(string $path): FileAttributes
+    public function mimeType(/*string */$path)/*: FileAttributes*/
     {
+        $path = cast_to_string($path);
+
         try {
             return $this->adapter->mimeType($this->prefix->prefixPath($path));
+        } catch (\Exception $previous) {
+        } catch (\ErrorException $previous) {
         } catch (Throwable $previous) {
-            throw UnableToRetrieveMetadata::mimeType($path, $previous->getMessage(), $previous);
+            // throw UnableToRetrieveMetadata::mimeType($path, $previous->getMessage(), $previous);
+        }
+
+        if (isset($previous)) {
+            throw new UnableToRetrieveMetadata($previous->getMessage(), $previous->getCode(), $previous);
         }
     }
 
-    public function visibility(string $path): FileAttributes
+    public function visibility(/*string */$path)/*: FileAttributes*/
     {
+        $path = cast_to_string($path);
+
         try {
             return $this->adapter->visibility($this->prefix->prefixPath($path));
+        } catch (\Exception $previous) {
+        } catch (\ErrorException $previous) {
         } catch (Throwable $previous) {
-            throw UnableToRetrieveMetadata::visibility($path, $previous->getMessage(), $previous);
+            // throw UnableToRetrieveMetadata::visibility($path, $previous->getMessage(), $previous);
+        }
+
+        if (isset($previous)) {
+            throw new UnableToRetrieveMetadata($previous->getMessage(), $previous->getCode(), $previous);
         }
     }
 
-    public function write(string $location, string $contents, Config $config): void
+    public function write(/*string */$location, /*string */$contents, Config $config)/*: void*/
     {
+        $location = cast_to_string($location);
+
+        $contents = cast_to_string($contents);
+
         try {
             $this->adapter->write($this->prefix->prefixPath($location), $contents, $config);
+        } catch (\Exception $previous) {
+        } catch (\ErrorException $previous) {
         } catch (Throwable $previous) {
-            throw UnableToWriteFile::atLocation($location, $previous->getMessage(), $previous);
+            // throw UnableToWriteFile::atLocation($location, $previous->getMessage(), $previous);
+        }
+
+        if (isset($previous)) {
+            throw new UnableToWriteFile($previous->getMessage(), $previous->getCode(), $previous);
         }
     }
 
-    public function writeStream(string $location, $contents, Config $config): void
+    public function writeStream(/*string */$location, $contents, Config $config)/*: void*/
     {
+        $location = cast_to_string($location);
+
         try {
             $this->adapter->writeStream($this->prefix->prefixPath($location), $contents, $config);
+        } catch (\Exception $previous) {
+        } catch (\ErrorException $previous) {
         } catch (Throwable $previous) {
-            throw UnableToWriteFile::atLocation($location, $previous->getMessage(), $previous);
+            // throw UnableToWriteFile::atLocation($location, $previous->getMessage(), $previous);
+        }
+
+        if (isset($previous)) {
+            throw new UnableToWriteFile($previous->getMessage(), $previous->getCode(), $previous);
         }
     }
 
-    public function setVisibility(string $path, string $visibility): void
+    public function setVisibility(/*string */$path, /*string */$visibility)/*: void*/
     {
+        $path = cast_to_string($path);
+
+        $visibility = cast_to_string($visibility);
+
         try {
             $this->adapter->setVisibility($this->prefix->prefixPath($path), $visibility);
+        } catch (\Exception $previous) {
+        } catch (\ErrorException $previous) {
         } catch (Throwable $previous) {
-            throw UnableToSetVisibility::atLocation($path, $previous->getMessage(), $previous);
+            // throw UnableToSetVisibility::atLocation($path, $previous->getMessage(), $previous);
+        }
+
+        if (isset($previous)) {
+            throw new UnableToSetVisibility($previous->getMessage(), $previous->getCode(), $previous);
         }
     }
 
-    public function delete(string $location): void
+    public function delete(/*string */$location)/*: void*/
     {
+        $location = cast_to_string($location);
+
         try {
             $this->adapter->delete($this->prefix->prefixPath($location));
+        } catch (\Exception $previous) {
+        } catch (\ErrorException $previous) {
         } catch (Throwable $previous) {
-            throw UnableToDeleteFile::atLocation($location, $previous->getMessage(), $previous);
+            // throw UnableToDeleteFile::atLocation($location, $previous->getMessage(), $previous);
+        }
+
+        if (isset($previous)) {
+            throw new UnableToDeleteFile($previous->getMessage(), $previous->getCode(), $previous);
         }
     }
 
-    public function deleteDirectory(string $location): void
+    public function deleteDirectory(/*string */$location)/*: void*/
     {
+        $location = cast_to_string($location);
+
         try {
             $this->adapter->deleteDirectory($this->prefix->prefixPath($location));
+        } catch (\Exception $previous) {
+        } catch (\ErrorException $previous) {
         } catch (Throwable $previous) {
-            throw UnableToDeleteDirectory::atLocation($location, $previous->getMessage(), $previous);
+            // throw UnableToDeleteDirectory::atLocation($location, $previous->getMessage(), $previous);
+        }
+
+        if (isset($previous)) {
+            throw new UnableToDeleteDirectory($previous->getMessage(), $previous->getCode(), $previous);
         }
     }
 
-    public function createDirectory(string $location, Config $config): void
+    public function createDirectory(/*string */$location, Config $config)/*: void*/
     {
+        $location = cast_to_string($location);
+
         try {
             $this->adapter->createDirectory($this->prefix->prefixPath($location), $config);
+        } catch (\Exception $previous) {
+        } catch (\ErrorException $previous) {
         } catch (Throwable $previous) {
-            throw UnableToCreateDirectory::atLocation($location, $previous->getMessage(), $previous);
+            // throw UnableToCreateDirectory::atLocation($location, $previous->getMessage(), $previous);
+        }
+
+        if (isset($previous)) {
+            throw new UnableToCreateDirectory($previous->getMessage(), $previous->getCode(), $previous);
         }
     }
 
-    public function move(string $source, string $destination, Config $config): void
+    public function move(/*string */$source, /*string */$destination, Config $config)/*: void*/
     {
+        $source = cast_to_string($source);
+
+        $destination = cast_to_string($destination);
+
         try {
             $this->adapter->move($this->prefix->prefixPath($source), $this->prefix->prefixPath($destination), $config);
+        } catch (\Exception $previous) {
+        } catch (\ErrorException $previous) {
         } catch (Throwable $previous) {
-            throw UnableToMoveFile::fromLocationTo($source, $destination, $previous);
+            // throw UnableToMoveFile::fromLocationTo($source, $destination, $previous);
+        }
+
+        if (isset($previous)) {
+            throw new UnableToMoveFile($previous->getMessage(), $previous->getCode(), $previous);
         }
     }
 
-    public function copy(string $source, string $destination, Config $config): void
+    public function copy(/*string */$source, /*string */$destination/*, Config $config*/)/*: void*/
     {
+        $source = cast_to_string($source);
+
+        $destination = cast_to_string($destination);
+
         try {
             $this->adapter->copy($this->prefix->prefixPath($source), $this->prefix->prefixPath($destination), $config);
+        } catch (\Exception $previous) {
+        } catch (\ErrorException $previous) {
         } catch (Throwable $previous) {
-            throw UnableToCopyFile::fromLocationTo($source, $destination, $previous);
+            // throw UnableToCopyFile::fromLocationTo($source, $destination, $previous);
         }
+
+        if (isset($previous)) {
+            throw new UnableToCopyFile($previous->getMessage(), $previous->getCode(), $previous);
+        }
+    }
+
+    public function update($path, $contents, Config $config)
+    {
+        return $this->adapter->update($this->prefix->prefixPath($path), $contents, $config);
+    }
+
+    public function updateStream($path, $resource, Config $config)
+    {
+        return $this->adapter->updateStream($this->prefix->prefixPath($path), $resource, $config);
+    }
+
+    public function rename($path, $newpath)
+    {
+        return $this->adapter->rename($this->prefix->prefixPath($path), $this->prefix->prefixPath($newpath));
+    }
+
+    public function deleteDir($dirname)
+    {
+        return $this->adapter->deleteDir($this->prefix->prefixPath($dirname));
+    }
+
+    public function createDir($dirname, Config $config)
+    {
+        return $this->adapter->createDir($this->prefix->prefixPath($dirname), $config);
+    }
+
+    public function has($path)
+    {
+        return $this->adapter->has($this->prefix->prefixPath($path));
+    }
+
+    public function getMetadata($path)
+    {
+        return $this->adapter->getMetadata($this->prefix->prefixPath($path));
+    }
+
+    public function getSize($path)
+    {
+        return $this->adapter->getSize($this->prefix->prefixPath($path));
+    }
+
+    public function getMimetype($path)
+    {
+        return $this->adapter->getMimetype($this->prefix->prefixPath($path));
+    }
+
+    public function getTimestamp($path)
+    {
+        return $this->adapter->getTimestamp($this->prefix->prefixPath($path));
+    }
+
+    public function getVisibility($path)
+    {
+        return $this->adapter->getVisibility($this->prefix->prefixPath($path));
     }
 }
--- cr/ext/league/flysystem/src/Patch/PathPrefixer.php
+++ cr/ext/league/flysystem/src/Patch/PathPrefixer.php
@@ -1,8 +1,8 @@
 <?php
 
-declare(strict_types=1);
+// declare(strict_types=1);
 
-namespace League\Flysystem;
+namespace League\Flysystem\Patch;
 
 use function rtrim;
 use function strlen;
@@ -20,8 +20,12 @@ final class PathPrefixer
      */
     private $separator = '/';
 
-    public function __construct(string $prefix, string $separator = '/')
+    public function __construct(/*string */$prefix, /*string */$separator = '/')
     {
+        $prefix = cast_to_string($prefix);
+
+        $separator = cast_to_string($separator);
+
         $this->prefix = rtrim($prefix, '\\/');
 
         if ($this->prefix !== '' || $prefix === $separator) {
@@ -31,24 +35,32 @@ final class PathPrefixer
         $this->separator = $separator;
     }
 
-    public function prefixPath(string $path): string
+    public function prefixPath(/*string */$path)/*: string*/
     {
+        $path = cast_to_string($path);
+
         return $this->prefix . ltrim($path, '\\/');
     }
 
-    public function stripPrefix(string $path): string
+    public function stripPrefix(/*string */$path)/*: string*/
     {
+        $path = cast_to_string($path);
+
         /* @var string */
         return substr($path, strlen($this->prefix));
     }
 
-    public function stripDirectoryPrefix(string $path): string
+    public function stripDirectoryPrefix(/*string */$path)/*: string*/
     {
+        $path = cast_to_string($path);
+
         return rtrim($this->stripPrefix($path), '\\/');
     }
 
-    public function prefixDirectoryPath(string $path): string
+    public function prefixDirectoryPath(/*string */$path)/*: string*/
     {
+        $path = cast_to_string($path);
+
         $prefixedPath = $this->prefixPath(rtrim($path, '\\/'));
 
         if ($prefixedPath === '' || substr($prefixedPath, -1) === $this->separator) {
