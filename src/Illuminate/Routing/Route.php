d33eb78ad0ac6612b9603ad6a107e0066f018e13
Saya <chu121su12@gmail.com>
2023-02-08 12:14:02 +0800

    revert using unsigned closure

--- src/Illuminate/Routing/Route.php
+++ src/Illuminate/Routing/Route.php
@@ -1350,13 +1350,13 @@ class Route
     {
         if ($this->action['uses'] instanceof Closure) {
             $this->action['uses'] = backport_serialize(
-                SerializableClosure::unsigned($this->action['uses'])
+                new SerializableClosure($this->action['uses'])
             );
         }
 
         if (isset($this->action['missing']) && $this->action['missing'] instanceof Closure) {
             $this->action['missing'] = backport_serialize(
-                SerializableClosure::unsigned($this->action['missing'])
+                new SerializableClosure($this->action['missing'])
             );
         }
 
