da1599a34ad5952e0a77c24857314aec838fca9d
Saya <chu121su12@gmail.com>
2024-02-12 13:46:40 +0800

    default db connect_timeout

--- src/Illuminate/Database/Connectors/Connector.php
+++ src/Illuminate/Database/Connectors/Connector.php
@@ -17,12 +17,12 @@ class Connector
      * @var array
      */
     protected $options = [
+        PDO::ATTR_TIMEOUT => 5,
         PDO::ATTR_CASE => PDO::CASE_NATURAL,
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_ORACLE_NULLS => PDO::NULL_NATURAL,
         PDO::ATTR_STRINGIFY_FETCHES => false,
         PDO::ATTR_EMULATE_PREPARES => false,
-        PDO::ATTR_TIMEOUT => 5,
     ];
 
     /**
